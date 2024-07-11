983fa98ffcef7f817bc21e45dc765cc87c3bb61a
themelektaus <melektaus@outlook.at>
2024-07-11 23:00:45 +0200

    QR Code Scanner

--- NewPage1.xaml.cs
+++ NewPage1.xaml.cs
@@ -1,5 +1,7 @@
 using Microsoft.Maui.Controls;
 
+using QRCoder;
+
 using System;
 
 using ZXing.Net.Maui;
@@ -8,9 +10,11 @@ namespace Brandagent;
 
 public partial class NewPage1 : ContentPage
 {
-	public NewPage1()
-	{
-		InitializeComponent();
+    public Data.Item Item { get; private set; }
+
+    public NewPage1()
+    {
+        InitializeComponent();
 
         cameraBarcodeReaderView.Options = new() { Formats = BarcodeFormats.TwoDimensional };
     }
@@ -19,7 +23,34 @@ public partial class NewPage1 : ContentPage
     {
         foreach (var barcode in e.Results)
         {
-            Console.WriteLine($"Barcodes: {barcode.Format} -> {barcode.Value}");
+            if (barcode.Format != BarcodeFormat.QrCode)
+                continue;
+
+            var totp = barcode.Value;
+
+            if (!totp.StartsWith("otpauth://totp/"))
+                continue;
+
+            var url = totp[15..].Split('?');
+
+            var parameters = System.Web.HttpUtility.ParseQueryString(url[1]);
+
+            var secret = parameters.Get("secret") ?? string.Empty;
+            if (secret == string.Empty)
+                continue;
+
+            Item = new()
+            {
+                service = string.Empty,
+                name = System.Web.HttpUtility.UrlDecode(url[0]),
+                secret = secret,
+                totp = totp,
+                qr = "data:image/png;base64," + Convert.ToBase64String(
+                    PngByteQRCodeHelper.GetQRCode(totp, QRCodeGenerator.ECCLevel.M, 6)
+                )
+            };
+
+            App.Current.MainPage.Navigation.PopModalAsync();
         }
     }
 }
